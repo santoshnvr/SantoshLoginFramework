cc5fa56e9cbb8f0e060f951171e24ea5befd876e
sxchanta <SXCHANTA@sentara.com>
2023-04-18 16:57:54 -0500

    Initial commit.

---
+++ SantoshLoginFramework/SantoshLoginFramework/SantoshLoginFramework.h
@@ -0,0 +1,18 @@
+//
+//  SantoshLoginFramework.h
+//  SantoshLoginFramework
+//
+//  Created by Santosh Kumar on 4/18/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SantoshLoginFramework.
+FOUNDATION_EXPORT double SantoshLoginFrameworkVersionNumber;
+
+//! Project version string for SantoshLoginFramework.
+FOUNDATION_EXPORT const unsigned char SantoshLoginFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SantoshLoginFramework/PublicHeader.h>
+
+
